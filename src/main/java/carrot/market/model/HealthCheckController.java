f2b963e64b05d91cae8ff2e1f7308b3351deac91
soochan <91618389+soochangoforit@users.noreply.github.com>
2023-01-04 12:18:48 +0900

    #6 feat : gitignore 파일을 gpg로 암호화 (#7)

---
+++ src/main/java/carrot/market/model/HealthCheckController.java
@@ -0,0 +1,15 @@
+package carrot.market.model;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public String checkHealth() {
+      return "healthy";
+    }
+
+}
