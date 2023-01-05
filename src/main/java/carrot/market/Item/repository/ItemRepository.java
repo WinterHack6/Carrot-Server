5f2537fb064800f4da4ec187ffdeb1281df4904f
ohjinseo <ojs835@naver.com>
2023-01-05 16:51:55 +0900

    #9 feat : 상품 게시글 로직 작성

--- src/main/java/carrot/market/Item/repository/ItemRepository.java
+++ src/main/java/carrot/market/Item/repository/ItemRepository.java
@@ -3,9 +3,13 @@ package carrot.market.Item.repository;
 import carrot.market.Item.domain.Item;
 import carrot.market.model.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
-    Optional<Item> findByIdAndStatus(Long itemId, Status active);
+
+    @Query("select i from Item i join fetch i.category c join fetch i.member m where i.id = :itemId and i.status = :active")
+    Optional<Item> findByIdAndStatus(@Param("itemId") Long itemId, @Param("active") Status active);
 }
