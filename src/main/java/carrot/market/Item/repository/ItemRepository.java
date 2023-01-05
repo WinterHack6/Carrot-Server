c43e88110811622954ebc3c7d6fcca9bf5a302bb
ohjinseo <62508156+ohjinseo@users.noreply.github.com>
2023-01-05 16:57:14 +0900

    Update ItemRepository.java
    fetch join으로 변경

--- src/main/java/carrot/market/Item/repository/ItemRepository.java
+++ src/main/java/carrot/market/Item/repository/ItemRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
-    Optional<Item> findByIdAndStatus(Long itemId, Status active);
+    @Query("select i from Item i join fetch i.category c join fetch i.member m where i.id = :itemId and i.status = :active")
+    Optional<Item> findByIdAndStatus(@Param("itemId") Long itemId, @Param("active") Status active);
 }
