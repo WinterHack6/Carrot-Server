7db5410c2d294bd01e648990335a869fc9ca4fd0
ohjinseo <ojs835@naver.com>
2023-01-06 14:12:34 +0900

    #16 feat : Swagger 문서 작성

--- src/main/java/carrot/market/Item/dto/ItemCreateRequestDto.java
+++ src/main/java/carrot/market/Item/dto/ItemCreateRequestDto.java
@@ -3,28 +3,34 @@ package carrot.market.Item.dto;
 import carrot.market.Item.domain.Item;
 import carrot.market.category.domain.Category;
 import carrot.market.member.domain.Member;
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Data
 @NoArgsConstructor
-@AllArgsConstructor
+@Getter
 public class ItemCreateRequestDto {
+    @ApiModelProperty(value = "상품 게시글 제목")
     private String title;
 
+    @ApiModelProperty(value = "유저 id")
     private Long memberId;
-
+    @ApiModelProperty(value = "상품 가격")
     private Integer price;
 
+    @ApiModelProperty(value = "상품 게시글 내용")
     private String content;
 
+    @ApiModelProperty(value = "나눔 여부")
     private Boolean isShare;
 
+    @ApiModelProperty(value = "가격 제안 여부")
     private Boolean isPriceOffer;
 
+    @ApiModelProperty(value = "카테고리 id")
     private Long categoryId;
 
+    @ApiModelProperty(value = "거래 희망 장소")
     private String hopePlace;
 
     public static Item toEntity(ItemCreateRequestDto req, Member member, Category category) {
