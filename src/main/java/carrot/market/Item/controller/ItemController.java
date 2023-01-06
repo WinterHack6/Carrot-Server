c214958d4525fc63e92c25ab33472729aea8f657
ohjinseo <62508156+ohjinseo@users.noreply.github.com>
2023-01-06 14:14:34 +0900

    #16 feat : Swagger 문서 작성 (#17)

--- src/main/java/carrot/market/Item/controller/ItemController.java
+++ src/main/java/carrot/market/Item/controller/ItemController.java
@@ -3,18 +3,22 @@ package carrot.market.Item.controller;
 import carrot.market.Item.dto.ItemCreateRequestDto;
 import carrot.market.Item.service.ItemService;
 import carrot.market.model.response.Response;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/items")
+@Api(tags="ItemController : 상품 게시글")
 @RestController
 public class ItemController {
     private final ItemService itemService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation(value="상품 게시글 추가", notes="상품 게시글을 추가할 수 있다.")
     public Response create(
             @RequestBody ItemCreateRequestDto req
     ) {
